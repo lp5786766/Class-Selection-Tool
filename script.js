868c1b113a4490e72e5f273dc7f5e9a77c5e046c
Luba Pecheneva <lp5786766@gmail.com>
2022-02-23 15:27:12 -0500

    button is disabled on click

--- script.js
+++ script.js
@@ -11,24 +11,52 @@ $(document).ready(function () {
   $('.collapsible').collapsible();
 });
 
+
+
 // As the user replies, we will append new questions to the page:
+const yesBtn = $('.yes');
+const noBtn = $('.no');
+const questionOne = $('#question1');
+const questionTwo = $('#question2');
+const questionThree = $('#question3');
+// show question 2
+
+// var instance = M.Collapsible.getInstance(elem);
+noBtn.on('click', function(event) {
+   
+    questionOne.removeClass('active');
+    questionThree.removeClass('hidden');
+    questionThree.addClass('active');
+    $('.collapsible').collapsible();
+});
 
 
+yesBtn.on('click', function (event) {
+    //  alert('clicked');
+    questionOne.removeClass('active').addClass('replied');
+    yesBtn.addClass('disabled');
+    noBtn.addClass('disabled');
+    questionTwo.removeClass('hidden');
+    questionTwo.addClass('active');
+    $('.collapsible').collapsible();
+});
+
+// show question 3
 
 
 //APPENDING A SECTION
 // const yesBtn = $('.yes');
-// const questionDiv = $(`  <li class="active list">
-// <div class="collapsible-header">
-//   <i class="material-icons">filter_drama</i>Question ${questionNumber} Title
+// const questionDiv = $(`  <li class='active list'>
+// <div class='collapsible-header'>
+//   <i class='material-icons'>filter_drama</i>Question ${questionNumber} Title
 // </div>
-// <div class="collapsible-body question">
-//   <div class="row">
+// <div class='collapsible-body question'>
+//   <div class='row'>
 //     <span>${question}</span>
 //   </div>
-//   <div class="row">
-//     <a class="waves-effect waves-light btn blue darken-2 yes">${replyOne}</a>
-//     <a class="waves-effect waves-light btn red darken-2 no">${replyTwo}</a>
+//   <div class='row'>
+//     <a class='waves-effect waves-light btn blue darken-2 yes'>${replyOne}</a>
+//     <a class='waves-effect waves-light btn red darken-2 no'>${replyTwo}</a>
 //   </div>
 // </div>
 // </li>`);
@@ -37,6 +65,6 @@ $(document).ready(function () {
 // console.log(questionDiv);
 
 // yesBtn.on('click', function (event) {
-//   //    alert("clicked");
+//   //    alert('clicked');
 //   list.append(questionDiv);
 // });
